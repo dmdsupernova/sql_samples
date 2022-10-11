9dd80cb0a7fc80411ae8437351c844fbce6d5791
Derek Donovan <115574616+dmdsupernova@users.noreply.github.com>
2022-10-11 14:02:38 -0400

    Create Queries.sql

---
+++ Cyclistic-Bike-Share/Queries.sql
@@ -0,0 +1,13 @@
+#standardSQL
+
+-- BigQuery sample SQL for Cyclistic Bike Share Data Analysis
+-- First step to merge data
+--
+--
+
+SELECT 
+      column1, column2, ...
+FROM 
+      table_name
+WHERE 
+      condition;
